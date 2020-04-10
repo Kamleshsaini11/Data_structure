18fabb1a0a3b6c4cbe6e97a8ef68541a4c0b2cc2
KAMLESH KUMAR SAINI <48117773+Kamleshsaini11@users.noreply.github.com>
2020-04-10 16:53:50 +0530

    Add files via upload

---
+++ BIT.cpp
@@ -0,0 +1,46 @@
+// fenwick tree or binary index tree
+#include<bits/stdc++.h>
+using namespace std;
+
+void updateBIT(int bit[],int size,int pos,int val);
+
+void create_bit(int arr[],int bit[],int size){
+   for(int i=1;i<size;i++){
+      updateBIT(bit,size,i,arr[i]);
+   }
+}
+
+void updateBIT(int bit[],int size,int pos,int val){      // val-> effective value means new value - previous value 
+   int j=pos;
+   while(size>j){
+      bit[j]+=val;
+      j+=j & -(j);
+   }
+}
+
+int getPrefixSum(int bit[],int pos){
+    int sum=0;
+    while(pos>0){
+       sum+=bit[pos];
+       pos-=pos & -(pos);
+    }
+    return sum;
+}
+
+int main()
+{
+   _fast_;
+   int arr[]={0,3,2,-1,6,5,4,-3,3,7,2,3};
+   int bit[12];
+   memset(bit,0,sizeof(bit));
+   create_bit(arr,bit,12);
+   for(int i=0;i<12;i++)
+      cout<<bit[i]<<" ";
+   cout<<getPrefixSum(bit,1)<<endl;
+}
+
+
+
+
+
+
